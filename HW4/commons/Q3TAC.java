20824b52e3487cead4c41d63380c4ddb92686de2
alireza <alireza.daghigh1999@gmail.com>
2021-01-01 21:42:58 +0330

    q3

--- HW4/commons/Q3TAC.java
+++ HW4/commons/Q3TAC.java
@@ -1,10 +1,10 @@
 main:
   BeginFunc 8;  //allocate 8 bytes for locals and temporary registers for function main
-  _t0 = (value for parameter x);
-  PushParam _t0;
-  PushParam c;
-  LCall _C.f;
-  PopParams 8;
+  _t0 = (value for parameter x); //save value of x in t0
+  PushParam _t0; //push t0 to stack frame
+  PushParam c; //push an object of C to stack frame
+  LCall _C.f; // Call function f()
+  PopParams 8; // pop parameters that we've been pushed in stack (t0 and c)
   EndFunc;
 
 
