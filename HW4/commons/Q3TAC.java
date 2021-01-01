0a814e55e6f0d1e0f72f5eca4b79230a71775722
Sepehr Feli <sepehr.feli.1377@gmail.com>
2021-01-01 21:17:54 +0330

    2.2

--- HW4/commons/Q3TAC.java
+++ HW4/commons/Q3TAC.java
@@ -1,6 +1,6 @@
 main:
   BeginFunc 8;  //allocate 8 bytes for locals and temporary registers for function main
-  _t0 = (value for parameter x); //
+  _t0 = (value for parameter x);
   PushParam _t0;
   PushParam c;
   LCall _C.f;
